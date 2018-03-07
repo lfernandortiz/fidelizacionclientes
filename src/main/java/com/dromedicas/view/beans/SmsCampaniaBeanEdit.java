c054074fcf557189bd31069be78ba70e9f9e0798
lfernandortiz <lfernandortiz@gmail.com>
2018-03-07 11:28:15 -0500

    Nuevos componentes de vista

--- src/main/java/com/dromedicas/view/beans/SmsCampaniaBeanEdit.java
+++ src/main/java/com/dromedicas/view/beans/SmsCampaniaBeanEdit.java
@@ -1,20 +1,178 @@
 package com.dromedicas.view.beans;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name="afiliadoBeanEdit")
+import com.dromedicas.domain.Sucursal;
+import com.dromedicas.service.CampaniaService;
+
+@ManagedBean(name="smsCampaniaBeanEdit")
 @SessionScoped
 public class SmsCampaniaBeanEdit {
+	
+	
+	private String nombreCampania;
+	private String audiencia;
+	private String queryStringMain;
+	private Date fechaInicio;
+	private Date fechaFin;
+	private String contenidoSms;
+	
+	private Sucursal sucursalSelected;
+	private String sexo;
+	private int edadIni;
+	private int edadFin;
+	private String[] selectedPatologias;
+	private List<String> Patologias;	
+
+	private String hijosmenoresde4;
+	private String hijosentre4y12;
+	private String hijosentre13y18;
+	private String hijosmayores;
 	
 	
 	public SmsCampaniaBeanEdit(){
 		
 	}
 	
-	
 	@PostConstruct
-	public void init(){}
+	public void init(){
+		
+	}
+	
+	
+	public String getNombreCampania() {
+		return nombreCampania;
+	}
+
+	public void setNombreCampania(String nombreCampania) {
+		this.nombreCampania = nombreCampania;
+	}
+		
+
+	public String getAudiencia() {
+		return audiencia;
+	}
+
+	public void setAudiencia(String audiencia) {
+		this.audiencia = audiencia;
+	}
+
+	public String getQueryStringMain() {
+		return queryStringMain;
+	}
+
+	public void setQueryStringMain(String queryStringMain) {
+		this.queryStringMain = queryStringMain;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public String getContenidoSms() {
+		return contenidoSms;
+	}
+
+	public void setContenidoSms(String contenidoSms) {
+		this.contenidoSms = contenidoSms;
+	}
+	
+	
+	public Sucursal getSucursalSelected() {
+		return sucursalSelected;
+	}
+
+	public void setSucursalSelected(Sucursal sucursalSelected) {
+		this.sucursalSelected = sucursalSelected;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public int getEdadIni() {
+		return edadIni;
+	}
+
+	public void setEdadIni(int edadIni) {
+		this.edadIni = edadIni;
+	}
+
+	public int getEdadFin() {
+		return edadFin;
+	}
+
+	public void setEdadFin(int edadFin) {
+		this.edadFin = edadFin;
+	}
+
+	public String[] getSelectedPatologias() {
+		return selectedPatologias;
+	}
+
+	public void setSelectedPatologias(String[] selectedPatologias) {
+		this.selectedPatologias = selectedPatologias;
+	}
+
+	public List<String> getPatologias() {
+		return Patologias;
+	}
+
+	public void setPatologias(List<String> patologias) {
+		Patologias = patologias;
+	}
+
+	public String getHijosmenoresde4() {
+		return hijosmenoresde4;
+	}
+
+	public void setHijosmenoresde4(String hijosmenoresde4) {
+		this.hijosmenoresde4 = hijosmenoresde4;
+	}
+
+	public String getHijosentre4y12() {
+		return hijosentre4y12;
+	}
+
+	public void setHijosentre4y12(String hijosentre4y12) {
+		this.hijosentre4y12 = hijosentre4y12;
+	}
+
+	public String getHijosentre13y18() {
+		return hijosentre13y18;
+	}
+
+	public void setHijosentre13y18(String hijosentre13y18) {
+		this.hijosentre13y18 = hijosentre13y18;
+	}
+
+	public String getHijosmayores() {
+		return hijosmayores;
+	}
+
+	public void setHijosmayores(String hijosmayores) {
+		this.hijosmayores = hijosmayores;
+	}
 
 }
--- src/main/java/com/dromedicas/view/beans/SmsCampaniaBeanList.java
+++ src/main/java/com/dromedicas/view/beans/SmsCampaniaBeanList.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import com.dromedicas.domain.Campania;
 import com.dromedicas.domain.Sucursal;
@@ -32,25 +33,8 @@ public class SmsCampaniaBeanList implements Serializable {
 	
 	private List<Campania> campaniaSMSList;
 	private String nombreCampania;
-	private String audiencia;
-	private String queryStringMain;
-	private Date fechaInicio;
-	private Date fechaFin;
-	private String contenidoSms;
-	
-	private Sucursal sucursalSelected;
-	private String sexo;
-	private int edadIni;
-	private int edadFin;
-	private String[] selectedPatologias;
-	private List<String> Patologias;	
-
-	private String hijosmenoresde4;
-	private String hijosentre4y12;
-	private String hijosentre13y18;
-	private String hijosmayores;	
-	
 		
+			
 	@PostConstruct
 	public void init(){
 				
@@ -63,7 +47,7 @@ public class SmsCampaniaBeanList implements Serializable {
 	public void setCampaniaSMSList(List<Campania> campaniaSMSList) {
 		this.campaniaSMSList = campaniaSMSList;
 	}
-	
+
 	public String getNombreCampania() {
 		return nombreCampania;
 	}
@@ -71,136 +55,6 @@ public class SmsCampaniaBeanList implements Serializable {
 	public void setNombreCampania(String nombreCampania) {
 		this.nombreCampania = nombreCampania;
 	}
-	
-	public CampaniaService getCampaniaService() {
-		return campaniaService;
-	}
-
-	public void setCampaniaService(CampaniaService campaniaService) {
-		this.campaniaService = campaniaService;
-	}
-
-	public String getAudiencia() {
-		return audiencia;
-	}
-
-	public void setAudiencia(String audiencia) {
-		this.audiencia = audiencia;
-	}
-
-	public String getQueryStringMain() {
-		return queryStringMain;
-	}
-
-	public void setQueryStringMain(String queryStringMain) {
-		this.queryStringMain = queryStringMain;
-	}
-
-	public Date getFechaInicio() {
-		return fechaInicio;
-	}
-
-	public void setFechaInicio(Date fechaInicio) {
-		this.fechaInicio = fechaInicio;
-	}
-
-	public Date getFechaFin() {
-		return fechaFin;
-	}
-
-	public void setFechaFin(Date fechaFin) {
-		this.fechaFin = fechaFin;
-	}
-
-	public String getContenidoSms() {
-		return contenidoSms;
-	}
-
-	public void setContenidoSms(String contenidoSms) {
-		this.contenidoSms = contenidoSms;
-	}
-	
-	
-	public Sucursal getSucursalSelected() {
-		return sucursalSelected;
-	}
-
-	public void setSucursalSelected(Sucursal sucursalSelected) {
-		this.sucursalSelected = sucursalSelected;
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
-	public int getEdadIni() {
-		return edadIni;
-	}
-
-	public void setEdadIni(int edadIni) {
-		this.edadIni = edadIni;
-	}
-
-	public int getEdadFin() {
-		return edadFin;
-	}
-
-	public void setEdadFin(int edadFin) {
-		this.edadFin = edadFin;
-	}
-
-	public String[] getSelectedPatologias() {
-		return selectedPatologias;
-	}
-
-	public void setSelectedPatologias(String[] selectedPatologias) {
-		this.selectedPatologias = selectedPatologias;
-	}
-
-	public List<String> getPatologias() {
-		return Patologias;
-	}
-
-	public void setPatologias(List<String> patologias) {
-		Patologias = patologias;
-	}
-
-	public String getHijosmenoresde4() {
-		return hijosmenoresde4;
-	}
-
-	public void setHijosmenoresde4(String hijosmenoresde4) {
-		this.hijosmenoresde4 = hijosmenoresde4;
-	}
-
-	public String getHijosentre4y12() {
-		return hijosentre4y12;
-	}
-
-	public void setHijosentre4y12(String hijosentre4y12) {
-		this.hijosentre4y12 = hijosentre4y12;
-	}
-
-	public String getHijosentre13y18() {
-		return hijosentre13y18;
-	}
-
-	public void setHijosentre13y18(String hijosentre13y18) {
-		this.hijosentre13y18 = hijosentre13y18;
-	}
-
-	public String getHijosmayores() {
-		return hijosmayores;
-	}
-
-	public void setHijosmayores(String hijosmayores) {
-		this.hijosmayores = hijosmayores;
-	}
-	
 
 	// ======================================
     // =       Metodos de la vista          =
@@ -209,9 +63,13 @@ public class SmsCampaniaBeanList implements Serializable {
 		
 	}
 	
+	
 	public String crearSMSCampania(){
-		return null;
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove("smsCampaniaBeanEdit");
+		return "smscampaniaedit?faces-redirect=true";	
 	}
+	
+	
 	
 	
 	public void buscarCampania(){
